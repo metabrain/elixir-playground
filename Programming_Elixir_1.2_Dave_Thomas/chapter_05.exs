392e284860db2e829758520f39f10fd42e1d5b85
metabrain <dany9995@gmail.com>
2016-04-14 19:24:49 +0000

    More chapter 05 exercises.

--- Programming_Elixir_1.2_Dave_Thomas/chapter_05.exs
+++ Programming_Elixir_1.2_Dave_Thomas/chapter_05.exs
@@ -4,4 +4,16 @@ defmodule Chapter_05 do
     l1 ++ l2
   end
 
+  def sum([]) do
+    0
+  end
+
+  def sum([h|t]) do
+    h + sum(t)
+  end
+
+  def pair_tuple_to_list({a,b}) do
+    [a,b]
+  end
+
 end
