ab17be56fb0c78795e0cd886ca35b97408f8b5e6
metabrain <dany9995@gmail.com>
2016-04-14 20:28:26 +0000

    More chapter 05 exercises (FizzBuzz).

--- Programming_Elixir_1.2_Dave_Thomas/chapter_05.exs
+++ Programming_Elixir_1.2_Dave_Thomas/chapter_05.exs
@@ -16,4 +16,12 @@ defmodule Chapter_05 do
     [a,b]
   end
 
+  def fizzbuzz(0,0,_), do: "FizzBuzz"
+  def fizzbuzz(0,_,_), do: "Fizz"
+  def fizzbuzz(_,0,_), do: "Buzz"
+  def fizzbuzz(_,_,x), do: x
+
+  def realFizzBuzz(n) do
+    fizzbuzz(rem(n,3), rem(n,5), n)
+  end
 end
--- Programming_Elixir_1.2_Dave_Thomas/test.exs
+++ Programming_Elixir_1.2_Dave_Thomas/test.exs
@@ -18,4 +18,26 @@ defmodule Test do
     assert Chapter_05.pair_tuple_to_list({1234,5678}) === [1234,5678]
   end
 
+  test "fizz_fizz" do
+    assert Chapter_05.fizzbuzz(0,1,1) === "Fizz"
+  end
+  test "fizz_buzz" do
+    assert Chapter_05.fizzbuzz(1,0,1) === "Buzz"
+  end
+  test "fizz_c" do
+    assert Chapter_05.fizzbuzz(1,1,6) === 6
+  end
+  test "fizz_buzzzz" do
+    assert Chapter_05.fizzbuzz(0,0,6) === "FizzBuzz"
+  end
+
+  test "real_fizzbuzz" do
+    assert Chapter_05.realFizzBuzz(10) == "Buzz"
+    assert Chapter_05.realFizzBuzz(11) == 11
+    assert Chapter_05.realFizzBuzz(12) == "Fizz"
+    assert Chapter_05.realFizzBuzz(13) == 13
+    assert Chapter_05.realFizzBuzz(14) == 14
+    assert Chapter_05.realFizzBuzz(15) == "FizzBuzz"
+    assert Chapter_05.realFizzBuzz(16) == 16
+  end
 end
